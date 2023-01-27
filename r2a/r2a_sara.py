c5e410efbcdffdc3a038895a2cea8f72022a1137
Thiago Cardoso <thiagocpais@hotmail.com>
2023-01-27 01:11:26 -0300

    adição de atributos para realização dos cálculos do algoritmo

--- r2a/r2a_sara.py
+++ r2a/r2a_sara.py
@@ -3,21 +3,28 @@
 from r2a.ir2a import IR2A
 from player.parser import *
 import time
+import math
 from statistics import harmonic_mean
+from base.whiteboard import *
 
 
 class R2A_Sara(IR2A):
 
     def __init__(self, id):
         IR2A.__init__(self, id)
+        self.wb = Whiteboard()
         self.throughputs = []
         self.request_time = 0
         self.qi = []
         self.weights = []
         self.h_mean = 0
-        self.initial = 2
-        self.alpha = 5
-        self.beta = 9
+        self.b_max = self.wb.get_max_buffer_size()
+        self.b_min = 2
+        self.b_alpha = math.floor(self.b_max*0.4)
+        self.b_beta = math.floor(self.b_max*0.8)
+        self.b_curr = 0
+        self.curr_seg = 0
+        
 
     def handle_xml_request(self, msg):
         self.request_time = time.perf_counter()
